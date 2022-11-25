60aefa7ea9b7585b503839fc14dbe2d40f047041
Lissandro Perossi Cunha <lissandro_cunha@yahoo.com.br>
2022-11-25 17:37:06 -0300

    added project str-producer

---
+++ str-producer/src/main/kotlin/br/com/lissandrocunha/kafka/strproducer/StrProducerApplication.kt
@@ -0,0 +1,11 @@
+package br.com.lissandrocunha.kafka.strproducer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StrProducerApplication
+
+fun main(args: Array<String>) {
+	runApplication<StrProducerApplication>(*args)
+}
---
+++ str-producer/src/test/kotlin/br/com/lissandrocunha/kafka/strproducer/StrProducerApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.lissandrocunha.kafka.strproducer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
