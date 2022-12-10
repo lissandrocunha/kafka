9e0a47c95c37ab40281f57d8bcc95ce49dfd8bfd
Lissandro Perossi Cunha <lissandro_cunha@yahoo.com.br>
2022-12-10 17:49:06 -0300

    create annotation for strConsumerCustomListener

---
+++ str-consumer/src/main/kotlin/br/com/lissandrocunha/kafka/strconsumer/custom/StrConsumerCustomListener.kt
@@ -0,0 +1,24 @@
+package br.com.lissandrocunha.kafka.strconsumer.custom
+
+import org.springframework.core.annotation.AliasFor
+import org.springframework.kafka.annotation.KafkaListener
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FUNCTION)
+@KafkaListener
+annotation class StrConsumerCustomListener(
+    @get:AliasFor(
+        annotation = KafkaListener::class,
+        attribute = "topics"
+    ) val topics: Array<String> = ["str-topic"],
+    @get:AliasFor(
+        annotation = KafkaListener::class,
+        attribute = "containerFactory"
+    ) val containerFactory: String = "strContainerFactory",
+    @get:AliasFor(
+        annotation = KafkaListener::class,
+        attribute = "groupId"
+    )val groupId:String = ""
+) {
+
+}
--- str-consumer/src/main/kotlin/br/com/lissandrocunha/kafka/strconsumer/listener/StrConsumerListener.kt
+++ str-consumer/src/main/kotlin/br/com/lissandrocunha/kafka/strconsumer/listener/StrConsumerListener.kt
@@ -1,7 +1,7 @@
 package br.com.lissandrocunha.kafka.strconsumer.listener
 
+import br.com.lissandrocunha.kafka.strconsumer.custom.StrConsumerCustomListener
 import org.apache.logging.log4j.LogManager
-import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
@@ -9,17 +9,17 @@ class StrConsumerListener {
 
     private val logger = LogManager.getLogger()
 
-    @KafkaListener(groupId = "group-0", topics = ["str-topic"], containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     fun listener(message: String){
         logger.info("LISTENER ::: Receive message {}", message)
     }
 
-    @KafkaListener(groupId = "group-1", topics = ["str-topic"], containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     fun log(message: String){
         logger.info("LOG ::: Receive message {}", message)
     }
 
-    @KafkaListener(groupId = "group-2", topics = ["str-topic"], containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-2")
     fun history(message: String){
         logger.info("HISTORY ::: Receive message {}", message)
     }
