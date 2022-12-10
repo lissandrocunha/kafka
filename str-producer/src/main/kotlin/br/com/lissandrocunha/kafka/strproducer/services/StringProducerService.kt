c0928b39ffa83be1b9cb61c736442687b03cd53a
Lissandro Perossi Cunha <lissandro_cunha@yahoo.com.br>
2022-12-09 22:59:39 -0300

    fix service producer

--- str-producer/src/main/kotlin/br/com/lissandrocunha/kafka/strproducer/services/StringProducerService.kt
+++ str-producer/src/main/kotlin/br/com/lissandrocunha/kafka/strproducer/services/StringProducerService.kt
@@ -1,8 +1,12 @@
 package br.com.lissandrocunha.kafka.strproducer.services
 
+import lombok.RequiredArgsConstructor
+import lombok.extern.log4j.Log4j2
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
+import kotlin.math.log
 
+@RequiredArgsConstructor
 @Service
 class StringProducerService(val kafkaTemplate: KafkaTemplate<String, String>) {
 
